8df0b0cdad5d0446355f1081c2726c17165d98fa
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-04-06 10:30:16 +0200

    hot reloading

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 module.exports = () => {
-    return {};
+    return {
+        afterRender(files, metalsmith, done) {
+            const webpackServer = files['scripts/webpack-server.js'];
+
+            if (webpackServer) {
+                files['scripts/webpack-server.js'].contents = new Buffer( //eslint-disable-line no-param-reassign
+                    webpackServer.contents.toString().replace('hot: false', 'hot: true')
+                );
+            }
+            done();
+        }
+    };
 };
