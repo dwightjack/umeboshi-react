919e3251d58758226264033009bd8ec4bfe264b4
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-04-11 11:03:13 +0200

    partials and hmr

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 module.exports = () => {
     return {
-        afterRender(files, metalsmith, done) {
-            const srcFiles = metalsmith.metadata().files;
-            const webpackServer = srcFiles['scripts/webpack-server.js'];
-
-            if (webpackServer) {
-                files['scripts/webpack-server.js'] = webpackServer; //eslint-disable-line no-param-reassign
-                files['scripts/webpack-server.js'].contents = new Buffer( //eslint-disable-line no-param-reassign
-                    webpackServer.contents.toString().replace('hot: false', 'hot: true')
-                );
-            }
+        beforeRender(files, metalsmith, done) {
+            const meta = metalsmith.metadata();
+            meta.tmpl.hot = true;
             done();
         }
     };
