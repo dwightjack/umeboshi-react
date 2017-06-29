9c73c7a31afb91027a7c5f8090e0255d03c2fc4d
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-06-29 15:47:16 +0200

    feat(entrypoint): new format
    moved to the new entrypoint format

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = () => {
-    return {
-        beforeRender(files, metalsmith, done) {
-            const meta = metalsmith.metadata();
-            meta.tmpl.hmr = true;
-            done();
-        }
-    };
+    console.error('This entrypoint format is deprecated. Please upgrade umeboshi-cli.');
+    console.error();
+    console.error('npm install -g umeboshi-cli');
+    return {};
+};
+
+module.exports.beforeRender = (files, metalsmith, done) => {
+    const meta = metalsmith.metadata();
+    meta.tmpl.hmr = true;
+    done();
 };
