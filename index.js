495baa2708d3fee542be8d6cf5e4ef0e0d237a8d
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-21 23:38:31 +0200

    fix(customizer): removed unused hook

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ module.exports = () => {
     console.error('npm install -g umeboshi-cli');
     return {};
 };
-
-module.exports.beforeRender = () => (files, metalsmith, done) => {
-    const meta = metalsmith.metadata();
-    meta.tmpl.hmr = true;
-    done();
-};
