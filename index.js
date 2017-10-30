6ec4b9f5cbf7dae1a169288968b30510a76f419a
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-30 22:53:01 +0100

    chore(repo): deprecation warning

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.warn('This template is DEPRECATED. Please upgrade umeboshi-cli to v3+ and use the new "webpack" template');
+
 module.exports = () => {
     console.error('This entrypoint format is deprecated. Please upgrade umeboshi-cli.');
     console.error();
